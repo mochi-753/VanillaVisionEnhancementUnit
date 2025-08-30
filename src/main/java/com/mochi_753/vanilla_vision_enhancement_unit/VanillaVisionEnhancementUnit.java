d6dfed574e7ff4cb3fe998c7ad106b17b88bead3
mochi-753 <naka.mochi11@gmail.com>
2025-08-30 22:55:47 +0900

    モジュールを作成

--- src/main/java/com/mochi_753/vanilla_vision_enhancement_unit/VanillaVisionEnhancementUnit.java
+++ src/main/java/com/mochi_753/vanilla_vision_enhancement_unit/VanillaVisionEnhancementUnit.java
@@ -1,15 +1,20 @@
 package com.mochi_753.vanilla_vision_enhancement_unit;
 
+import com.mochi_753.vanilla_vision_enhancement_unit.register.ModItems;
+import com.mochi_753.vanilla_vision_enhancement_unit.register.ModModules;
+import mekanism.api.gear.ICustomModule;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(VanillaVisionEnhancementUnit.MOD_ID)
-public class VanillaVisionEnhancementUnit {
+public class VanillaVisionEnhancementUnit implements ICustomModule<VanillaVisionEnhancementUnit> {
     public static final String MOD_ID = "vanilla_vision_enhancement_unit";
 
     @SuppressWarnings("removal")
     public VanillaVisionEnhancementUnit() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(bus);
+        ModModules.register(bus);
     }
 }
